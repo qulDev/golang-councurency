e06e3c4fed5b018cbe8a27d25d159111dc4a84d5
qulDev <mm.rizzqullah@gmail.com>
2025-09-27 22:50:36 +0800

    add .gitignore and initial test for Hello World function

---
+++ helper/goroutine_test.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello, World!")
+}
+
+func TestRunHelloWorld(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("ups")
+
+	time.Sleep(1 * time.Second)
+}
