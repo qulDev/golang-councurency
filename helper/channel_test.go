9d7615a87b5daa787478624197b653e46e491fcb
qulDev <mm.rizzqullah@gmail.com>
2025-09-28 18:15:46 +0800

    test: add test for channel as parameter with goroutine response

--- helper/channel_test.go
+++ helper/channel_test.go
@@ -1,6 +1,10 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestCreateChannel(t *testing.T) {
 	ch := make(chan string)
@@ -8,4 +12,22 @@ func TestCreateChannel(t *testing.T) {
 	ch <- "Test Channel"
 
 	close(ch)
+}
+
+func GiveMeRespond(ch chan string) {
+	time.Sleep(2 * time.Second)
+	ch <- "Rizqullah"
+	fmt.Println("Selesai")
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	ch := make(chan string)
+	defer close(ch)
+
+	go GiveMeRespond(ch)
+	fmt.Println("Menunggu...")
+	res := <-ch
+	fmt.Println(res)
+
+	time.Sleep(3 * time.Second)
 }
