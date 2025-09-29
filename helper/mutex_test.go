88c8481f4e4c3bda1d603c527370da298d09c6a9
qulDev <mm.rizzqullah@gmail.com>
2025-09-29 22:24:58 +0800

    test: add tests for RWMutex and deadlock scenarios in bank account handling

--- helper/mutex_test.go
+++ helper/mutex_test.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"log"
 	"sync"
 	"testing"
 	"time"
@@ -21,4 +22,90 @@ func TestMutex(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 	t.Logf("Counter: %d", x)
+}
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (b *BankAccount) AddBalance(amount int) {
+	b.RWMutex.Lock()
+	b.Balance = b.Balance + amount
+	b.RWMutex.Unlock()
+}
+
+func (b *BankAccount) GetBalance() int {
+	b.RWMutex.RLock()
+	balance := b.Balance
+	b.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+	for i := 1; i <= 100; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				account.AddBalance(1)
+				t.Logf("Balance: %d", account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	t.Logf("Final Balance: %d", account.GetBalance())
+}
+
+type UserBelance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBelance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBelance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBelance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Tranfer(user1, user2 *UserBelance, amount int) {
+	user1.Lock()
+	log.Println("Lock", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	log.Println("Lock", user2.Name)
+	user2.Change(amount)
+
+	user1.Unlock()
+	user2.Unlock()
+	log.Println("Unlock", user1.Name)
+	log.Println("Unlock", user2.Name)
+}
+
+func TestDeadLock(t *testing.T) {
+	userA := UserBelance{
+		Name:    "Budi",
+		Balance: 100000,
+	}
+
+	userB := UserBelance{
+		Name:    "Eko",
+		Balance: 100000,
+	}
+
+	go Tranfer(&userA, &userB, 10000)
+	go Tranfer(&userB, &userA, 20000)
+
+	time.Sleep(10 * time.Second)
+	log.Println("User A Balance", userA.Balance)
+	log.Println("User B Balance", userB.Balance)
 }
