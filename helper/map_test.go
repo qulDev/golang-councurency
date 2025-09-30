3bb179a7087af5c92d06abeb529616eb13be05eb
qulDev <mm.rizzqullah@gmail.com>
2025-09-30 23:30:03 +0800

    test: add initial test for concurrent map operations in helper package

--- helper/map_test.go
+++ helper/map_test.go
@@ -1,1 +1,30 @@
 package helper
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func AddToMap(wg *sync.WaitGroup, data *sync.Map, value int) {
+	defer wg.Done()
+
+	data.Store(value, value)
+}
+
+func TestMap(t *testing.T) {
+	var data sync.Map
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go AddToMap(&wg, &data, i)
+	}
+
+	wg.Wait()
+
+	data.Range(func(key, value any) bool {
+		fmt.Println(key, ":", value)
+		return true
+	})
+}
